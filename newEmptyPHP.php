f4e8e1d7c9bdb5b1f779a5141cb3569842b85b09
lntquang1 <lenguyenthanhquang@gmail.com>
2012-10-18 20:05:05 +0700

    Update newEmptyPHP.php

--- newEmptyPHP.php
+++ newEmptyPHP.php
@@ -3,6 +3,7 @@ echo"chao";
 echo"chao1";
 echo"chao 2";
 echo"chao 3 from clone";
+echo "chao ban from server";
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
