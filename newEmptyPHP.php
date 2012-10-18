cbb0fdd0800779df789f8be89d7c7f8fd8feedb2
lntquang <lntquang_uns@yahoo.com>
2012-10-18 20:06:26 +0700

    tao vua edit xong

--- newEmptyPHP.php
+++ newEmptyPHP.php
@@ -4,6 +4,7 @@ echo"chao1";
 echo"chao 2";
 echo"chao 3 from clone";
 echo "chao ban from server";
+echo"chao ban from client";
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
